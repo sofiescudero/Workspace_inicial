4175332d5830228a2e7687c07ea966cc1c82d90d
Sofía Escudero <sofiescudero@users.noreply.github.com>
2020-08-15 23:00:26 -0300

    Revert "Google Auth2"
    This reverts commit 8286fb240221d227d22094890b84e9d919fd72c0.

--- js/login.js
+++ js/login.js
@@ -11,16 +11,4 @@ document.addEventListener("DOMContentLoaded", function(e){
        sessionStorage.setItem("logged") = true;
        return true;
    })
-   function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    event.preventDefault();
-    location.href = index.html
-    sessionStorage.setItem("logged") = true;
-    return true;
-  }
-  
 });
